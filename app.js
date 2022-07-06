859b709e1f68c696dde8e083c600f40978a2486c
Daeyeon Cho <107440788+daecho8@users.noreply.github.com>
2022-07-06 11:48:49 -0500

    Final one with two charts

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 //started
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
+const ctx = document.getElementById('chartOne').getContext('2d');
+const chartOne = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Python', 'Java', 'JavaScript', 'C#', 'C/C++'],
+        labels: ['Java', 'C', 'Python', 'C++', 'C#'],
         datasets: [{
-            label: 'JUL 2022', 
-            data: [28.38, 17.5, 9.29, 7.63, 6.48],
+            label: 'TIOBE', 
+            data: [16.89, 15.77, 9.7, 5.5, 5.3],
             backgroundColor: [
-                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
@@ -35,3 +35,38 @@ const myChart = new Chart(ctx, {
     }
 });
 
+const ctx02 = document.getElementById('chartTwo').getContext('2d');
+const chartTwo = new Chart(ctx02, {
+    type: 'polarArea',
+    data: {
+        labels: ['Python', 'Java', 'Javascript', 'C#', 'PHP'],
+        datasets: [{
+            label: 'PYPL', 
+            data: [29.72, 19.03, 8.2, 7.28, 6.09],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)'
+
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)'
+
+            ],
+            borderWidth: 3
+        }],
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
